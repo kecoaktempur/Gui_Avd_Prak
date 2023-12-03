3f9c8608120dd68c40ebfa9ffc195e1407688bd7
kecoaktempur <triwan98@gmail.com>
2023-12-03 13:36:01 +0700

    setup static done

--- Project_AVD/settings.py
+++ Project_AVD/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Define the location of your static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'GUI_AVD/static'),
+]
+
+# Define the location for collectstatic to gather static files for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
